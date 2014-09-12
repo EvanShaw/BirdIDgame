88f2e2c54e8057965258bf8903c67b9e78653f67
Evan Shaw <evandanielshaw@gmail.com>
2014-09-12 12:54:23 -0500

    added java files to the recreated src eclipse folder

---
+++ CurlewProject/src/Bird.java
@@ -0,0 +1,14 @@
+
+public class Bird {
+	String imagePath;
+	String birdName;
+	
+	public Bird(String imagePath, String birdName) {
+		this.imagePath = imagePath;
+		this.birdName = birdName;
+	}
+	
+	public boolean isMatching(String birdChosen) {
+		return birdName.equals(birdChosen);
+	}
+}
