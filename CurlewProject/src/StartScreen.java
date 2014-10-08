04d97c506969be08850fd8c25be67856aab772ed
Kyle <Kylehutchison12@augustana.edu>
2014-10-08 01:15:08 -0500

    Finished start screen GUI.

--- CurlewProject/src/StartScreen.java
+++ CurlewProject/src/StartScreen.java
@@ -24,34 +24,48 @@ public class StartScreen extends JFrame implements ActionListener{
 		newUser = new JButton("Create new username");
 		hard = new JButton("Hard");
 		easy = new JButton("Easy");
-		userName = new JTextField("Enter username");
+		userName = new JTextField("Enter Username");
 		enterUserName = new JLabel(
-				"Enter username. If this your first time click the create new username button");
-		//Container center = new Container();
-		//center.setLayout(new GridLayout());
+				"If this your first time click the create new username button",SwingConstants.CENTER);
+		
+		
 
-		this.setSize(600, 500);
+		this.setSize(500,200);
 		this.setLayout(new BorderLayout());
 	
 		//setting the panels and layouts
 		Container contain = this.getContentPane();
 		contain.setLayout(new BorderLayout());
-		JPanel difficultyPanel = new JPanel(new FlowLayout());
-		contain.add(difficultyPanel, BorderLayout.SOUTH);
-		//contain.add(newUser, BorderLayout.NORTH);
-		contain.add(enterUserName, BorderLayout.CENTER);
-		contain.add(userName, BorderLayout.NORTH);
-		difficultyPanel.add(easy);
-		difficultyPanel.add(hard);
 		
-		easy.addActionListener(this);
-		hard.addActionListener(this);
+		
+		contain.add(new JLabel("Welcome To Curlew's Bird Identification Quiz"),BorderLayout.NORTH);
+		
+		
+		JPanel center=new JPanel(new GridLayout(2,0));
+		center.add(userName);
+		center.add(enterUserName);
+		
+		
+		JPanel userBtnPanel= new JPanel(new FlowLayout());
+		contain.add(userBtnPanel,BorderLayout.SOUTH);
+		contain.add(center,BorderLayout.CENTER);
+		
+		
+		userBtnPanel.add(logIn);
+		userBtnPanel.add(newUser);
+		
+		
+		
+		
+		
+		logIn.addActionListener(this);
+		newUser.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		JButton sourceButton = (JButton) event.getSource();
-		chosenDifficulty.setDifficulty(sourceButton.getText());
+		
 		
 		/* TODO: Temporary invocation of BirdIdGUI. Ideally we want only the user fields and information to appear 
 		 * first, then after the user information is entered, a box asking the user what difficulty they would
