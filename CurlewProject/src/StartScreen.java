3c966f2908d403483dddf4cdd983d01373cb93bf
Kyle <Kylehutchison12@augustana.edu>
2014-10-08 22:21:20 -0500

    added the difficulty GUI

--- CurlewProject/src/StartScreen.java
+++ CurlewProject/src/StartScreen.java
@@ -4,60 +4,50 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class StartScreen extends JFrame implements ActionListener{
-
+public class StartScreen extends JFrame implements ActionListener {
+		//fields
 	private User user;
 	private Difficulty chosenDifficulty;
 	private JButton logIn;
 	private JButton newUser;
-	private JButton hard;
-	private JButton easy;
+
 	private JTextField userName;
 	private JLabel enterUserName;
-	
+
 	public StartScreen() {
 		super("StartScreen");
-		
+
 		user = new User();
 		chosenDifficulty = new Difficulty();
 		logIn = new JButton("Log in");
 		newUser = new JButton("Create new username");
-		hard = new JButton("Hard");
-		easy = new JButton("Easy");
+
 		userName = new JTextField("Enter Username");
 		enterUserName = new JLabel(
-				"If this your first time click the create new username button",SwingConstants.CENTER);
-		
-		
+				"If this your first time click the create new username button",
+				SwingConstants.CENTER);
 
-		this.setSize(500,200);
+		this.setSize(500, 200);
 		this.setLayout(new BorderLayout());
-	
-		//setting the panels and layouts
+
+		// setting the panels and layouts
 		Container contain = this.getContentPane();
 		contain.setLayout(new BorderLayout());
-		
-		
-		contain.add(new JLabel("Welcome To Curlew's Bird Identification Quiz"),BorderLayout.NORTH);
-		
-		
-		JPanel center=new JPanel(new GridLayout(2,0));
+
+		contain.add(new JLabel("Welcome To Curlew's Bird Identification Quiz"),
+				BorderLayout.NORTH);
+
+		JPanel center = new JPanel(new GridLayout(2, 0));
 		center.add(userName);
 		center.add(enterUserName);
-		
-		
-		JPanel userBtnPanel= new JPanel(new FlowLayout());
-		contain.add(userBtnPanel,BorderLayout.SOUTH);
-		contain.add(center,BorderLayout.CENTER);
-		
-		
+
+		JPanel userBtnPanel = new JPanel(new FlowLayout());
+		contain.add(userBtnPanel, BorderLayout.SOUTH);
+		contain.add(center, BorderLayout.CENTER);
+
 		userBtnPanel.add(logIn);
 		userBtnPanel.add(newUser);
-		
-		
-		
-		
-		
+
 		logIn.addActionListener(this);
 		newUser.addActionListener(this);
 	}
@@ -65,14 +55,16 @@ public class StartScreen extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		JButton sourceButton = (JButton) event.getSource();
+		if (sourceButton == logIn) {
+			/*
+			 * TODO some sort of check to make sure it is saved
+			 */
+			
+			DifficultyGui setDifficulty = new DifficultyGui();
+		}else{
+			
+		}
+
 		
-		
-		/* TODO: Temporary invocation of BirdIdGUI. Ideally we want only the user fields and information to appear 
-		 * first, then after the user information is entered, a box asking the user what difficulty they would
-		 * like will display, and then THAT will invoke BirdIdGUI.
-		 */
-		BirdLibrary library = new BirdLibrary();
-		BirdIdGUI myFrame = new BirdIdGUI(library.getLibrary(), chosenDifficulty);
-		myFrame.setVisible(true);
 	}
 }
