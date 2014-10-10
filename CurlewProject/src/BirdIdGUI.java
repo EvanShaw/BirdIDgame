d9a54f840379916dd2f6a5b0c394181cc29f04c9
Evan Shaw <evandanielshaw@gmail.com>
2014-10-10 13:43:02 -0500

    Vastly improve implementation of Collections.shuffle and equality checking. ResetAfterAction is still very broken.

--- CurlewProject/src/BirdIdGUI.java
+++ CurlewProject/src/BirdIdGUI.java
@@ -16,79 +16,69 @@ public class BirdIdGUI extends JFrame implements ActionListener {
 	private Difficulty chosenDifficulty;
 	private JLabel scoreLabel;
 	private ArrayList<Bird> birds;
-	private Bird[] birdsUsed;
 	private Bird currentBird;
-	private int sumOfQuestions; 
+	private int sumOfQuestions;
+	private int correctBirdIndex;
 	private ArrayList<JButton> buttons;
 	private JLabel imageLabel;
-	   
+
 	public BirdIdGUI(ArrayList<Bird> birds, Difficulty chosenDifficulty) {
 		super("BirdIdGUI");
-		
-		Collections.shuffle(birds);
-		
+
 		this.birds = birds;
 		this.chosenDifficulty = chosenDifficulty;
 		sessionScore = new Score();
 		scoreLabel = new JLabel("Score: " + sessionScore.getScore());
+		imageLabel = new JLabel();
 		sumOfQuestions = 0;
-		currentBird = birds.remove(0);
 		buttons = new ArrayList<JButton>();
-		
-		//basic window operations
+
+		// basic window operations
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(600, 500);
 
-		//setting the panels and layouts
+		// setting the panels and layouts
 		Container contain = this.getContentPane();
 		contain.setLayout(new BorderLayout());
 		JPanel southButtonPanel = new JPanel(new FlowLayout());
 		contain.add(southButtonPanel, BorderLayout.SOUTH);
 		contain.add(scoreLabel, BorderLayout.NORTH);
+		imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contain.add(imageLabel, BorderLayout.CENTER);
 
-		//creating the buttons
-		
+		// creating the buttons
 		for (int i = 0; i < chosenDifficulty.getNumButtons(); i++) {
 			buttons.add(new JButton());
 			southButtonPanel.add(buttons.get(i));
 			buttons.get(i).addActionListener(this);
 		}
-		
-		buttonPopulator();
-		
-		//adding images
-		imageLabel = new JLabel();
-		imageLabel.setIcon(new ImageIcon("src/thumb_"+currentBird.getImagePath()));
-		imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		contain.add(imageLabel, BorderLayout.CENTER);
-		this.validate();
+
+		displayQuestion();
 	}
 
 	/**
-	 * Accesses Birds from the list randomly to create the button labels for each question.
+	 * Accesses Birds from the list randomly to create the button labels for
+	 * each question.
 	 */
-	public void buttonPopulator(){
-		
+	public void displayQuestion() {
+
 		Random rand = new Random();
-		int correctButton = rand.nextInt(chosenDifficulty.getNumButtons());
-		
-		boolean containsAnswer = false;
+		Collections.shuffle(birds);
+		correctBirdIndex = rand.nextInt(chosenDifficulty.getNumButtons());
+		currentBird = birds.get(correctBirdIndex);
+
+		imageLabel.setIcon(new ImageIcon("src/thumb_"
+				+ currentBird.getImagePath()));
+
 		for (int i = 0; i < chosenDifficulty.getNumButtons(); i++) {
-			if (!birds.get(i).equals(currentBird)) {
-				buttons.get(i).setText(birds.get(i).getBirdName());
-			} else {
-				buttons.get(i).setText(birds.get(i).getBirdName());
-				containsAnswer = true;
-			}
+			buttons.get(i).setText(birds.get(i).getBirdName());
 		}
-		
-		if (!containsAnswer) {
-			buttons.get(correctButton).setText(currentBird.getBirdName());
-		}
+
 	}
 
 	/**
 	 * Handles the action listening for all GUI button interaction.
+	 * 
 	 * @Override
 	 */
 	public void actionPerformed(ActionEvent event) {
@@ -101,44 +91,56 @@ public class BirdIdGUI extends JFrame implements ActionListener {
 
 		} else {
 			sourceButton.setBackground(Color.RED);
-				
+
 		}
+
+		/*
+		 * Remove the correct bird from the array so that it is not randomly
+		 * selected in the future
+		 */
+		birds.remove(correctBirdIndex);
+		Collections.shuffle(birds);
+
+		sumOfQuestions++;
 		if (sumOfQuestions == chosenDifficulty.getNumQuestions()) {
 			endProgram();
 		} else {
 			resetAfterAction(sourceButton, defaultColor);
 		}
-		currentBird = birds.remove(0);
 	}
 
 	/**
-	 * Displays the final result screen of the program after the questions are done.
+	 * Displays the final result screen of the program after the questions are
+	 * done.
 	 */
 	public void endProgram() {
-		
-			scoreLabel.setText("You Answered "+sessionScore.getScore()+"/"+chosenDifficulty.getNumQuestions()+"correctly");
-			
+
+		scoreLabel.setText("You Answered " + sessionScore.getScore() + "/"
+				+ chosenDifficulty.getNumQuestions() + "correctly");
+
 	}
 
 	/**
-	 * When there are more questions to come, this method resets the screen for the
-	 * new question and calls populate() to re-populate the buttons.
-	 * @params srcBtn, defaultColor - uses the variables from the actionPerformed
-	 * 		method to reset the changed button to its original color
+	 * When there are more questions to come, this method resets the screen for
+	 * the new question and calls populate() to re-populate the buttons.
+	 * 
+	 * @params srcBtn, defaultColor - uses the variables from the
+	 *         actionPerformed method to reset the changed button to its
+	 *         original color
 	 */
 	public void resetAfterAction(final JButton srcBtn, final Color defaultColor) {
 		Timer timer = new Timer(2000, new ActionListener() {
-		    public void actionPerformed(ActionEvent event) {
-				currentBird = birds.get(sumOfQuestions);
-				imageLabel.setIcon(new ImageIcon("src/thumb_"+currentBird.getImagePath()));
+			public void actionPerformed(ActionEvent event) {
 				srcBtn.setBackground(defaultColor);
-				//TODO set all buttons using data fields to default color, not just source
-				Collections.shuffle(birds);
-				buttonPopulator();
-		    }
+				// TODO set all buttons using data fields to default color, not
+				// just source
+			}
 		});
+		/*
 		timer.setRepeats(false);
 		timer.start();
-		timer.stop();
+		timer.stop(); 
+		*/
+		displayQuestion();
 	}
 }
